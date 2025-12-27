982824b51e24c1cfd4e4a99cae094d1675dd965c
Tahasin <110173793+tahasintonmoy2@users.noreply.github.com>
2025-12-27 19:54:26 +0600

    feat: remove unused imports and add global type declarations for CSS and SCSS modules

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import type React from "react";
-//@ts-expect-error import localFont from "next/font/local";
 import "./globals.css";
 
 const lin = Roboto({ subsets: ["latin"], weight: ["400"] });
--- components/about.tsx
+++ components/about.tsx
@@ -2,7 +2,6 @@
 
 import gsap from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
-import { useRef } from "react"
 
 gsap.registerPlugin(ScrollTrigger)
 
